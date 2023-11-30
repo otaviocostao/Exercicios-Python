8e2900645e3af4b4e3c8c122a4f079901cf425a5
Otavio Costa <dev.otaviocostao@gmail.com>
2023-11-29 22:47:46 -0300

    Correção na listadecompras.py
    Criação das condições 'if'
    Alteração do 'while'
    Criação das variáveis

--- listadecompras.py
+++ listadecompras.py
@@ -1,12 +1,20 @@
 #ENUNCIADO: Faça uma lista de compras. O usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista.
+lista_de_compras= []
+lista_enumerada = list(enumerate(lista_de_compras))
 
-opcaoinicial = input("Selecione uma opção:\n[I]nserir [A]pagar [L]istar:\n").lower()
+while True:
+    opcaoinicial = input("Selecione uma opção:\n[I]nserir [A]pagar [L]istar:\n").lower()
 
-while opcaoinicial == 'i':
-    pass
+    if opcaoinicial == 'i':
+        produto = input('Qual produto deseja adicionar a lista? ')
+        lista_de_compras.append(produto)
+        print(lista_de_compras)
 
-while opcaoinicial == 'a':
-    pass
+    if opcaoinicial == 'a':
+        pos_apagar = int(input('Digite o índice que deseja apagar: '))
+        lista_de_compras.pop(pos_apagar)
+        print(lista_de_compras)
 
-while opcaoinicial == 'l':
-    pass
+    if opcaoinicial == 'l':
+        for indice, produto in lista_enumerada:
+            print(lista_enumerada)
