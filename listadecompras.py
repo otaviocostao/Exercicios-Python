91f796da61c553ba313179929241c4099f8d49eb
Otavio Costa <dev.otaviocostao@gmail.com>
2023-11-29 22:24:27 -0300

    Criei estrutura do While

--- listadecompras.py
+++ listadecompras.py
@@ -1,2 +1,12 @@
 #ENUNCIADO: Faça uma lista de compras. O usuário deve ter a possibilidade de inserir, apagar e listar valores da sua lista.
 
+opcaoinicial = input("Selecione uma opção:\n[I]nserir [A]pagar [L]istar:\n").lower()
+
+while opcaoinicial == 'i':
+    pass
+
+while opcaoinicial == 'a':
+    pass
+
+while opcaoinicial == 'l':
+    pass
