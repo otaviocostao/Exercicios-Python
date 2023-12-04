4ee1b225d9390596956fe1b9a8143c2989686500
Otavio Costa <dev.otaviocostao@gmail.com>
2023-12-04 15:08:45 -0300

    Update split.py

--- split.py
+++ split.py
@@ -4,3 +4,11 @@ join: une uma string
 """
 
 frase = 'Olha só, que coisa interessante'
+lista_frases_cruas= frase.split(',') #Divide as frases pela vírgula
+
+lista_frases = []
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+print(lista_frases_cruas) #Com o espaço
+print(lista_frases) #Sem o espaço
