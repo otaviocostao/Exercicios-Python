94dc4f81a3c36b3ac424dbfc6bfa14b472acbdcd
Otavio Costa <dev.otaviocostao@gmail.com>
2023-12-15 15:37:22 -0300

    Resposta dos exercícios

--- exercicio-com-funcoes.py
+++ exercicio-com-funcoes.py
@@ -1,2 +1,33 @@
 # Exercícios com funções
 # Crie funções que duplicam, triplicam e quadruplicam o número recebido como parâmetro.
+
+def duplicar(numero):
+    return numero * 2
+
+def triplicar(numero):
+    return numero * 3
+
+def quadruplicar(numero):
+    return numero * 4
+
+print('EXERCÍCIO 1:')
+numero_input = int(input('Digite um numero: '))
+
+print(duplicar(numero_input))
+print(triplicar(numero_input))
+print(quadruplicar(numero_input))
+
+# Crie uma função que multiplica um numero por um multiplicador
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+print('EXERCÍCIO 2:')
+numero_input2 = int(input('Digite um número para ser multiplicado: '))    
+multiplicador_input = int(input(f'Digite o multiplicador do número {numero_input2}: '))
+
+multiplicador = criar_multiplicador(multiplicador_input)
+
+print(multiplicador(numero_input2))
