7a435ef52093b56ee0108cda9dc86b096888b6bb
Otavio Costa <dev.otaviocostao@gmail.com>
2023-12-14 22:05:12 -0300

    Update exercicio-funcoes.py

--- exercicio-funcoes.py
+++ exercicio-funcoes.py
@@ -4,3 +4,14 @@
 # não nomeados recebidos
 # Retorne o total para uma variável e mostre o valor
 # da variável.
+
+def multiplicar(*args):
+    total = 1
+    for numero in args:
+        total *= numero
+    return total
+
+valores = multiplicar(1, 2, 3, 4, 5)
+
+print(valores)
+
